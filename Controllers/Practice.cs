c21a268997942c4b4ba2fca9995d4ca05dd638cd
RAGREON <ragreon809@gmail.com>
2026-02-18 15:37:20 +0545

    Add Practice Controller

---
+++ Controllers/Practice.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PracticeController : ControllerBase
+{
+  public PracticeController()
+  {
+  }
+
+  [HttpGet]
+  public IActionResult GetTime()
+  {
+    var currentTime = DateTime.UtcNow;
+
+    return Ok(currentTime);
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
 var app = builder.Build();
+
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
