9e0e4c8983d5a44bc587e3fb9fea66b497f868b7
RAGREON <ragreon809@gmail.com>
2026-02-20 00:58:05 +0545

    change Host:localhost to Host=localhost

--- Data/AppDbContextFactory.cs
+++ Data/AppDbContextFactory.cs
@@ -6,7 +6,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
   public AppDbContext CreateDbContext(string[] args)
   {
     var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-    optionsBuilder.UseNpgsql("Host:localhost;Database=MigrationBuildOnly;");
+    optionsBuilder.UseNpgsql("Host=localhost;Database=MigrationBuildOnly;");
 
     return new AppDbContext(optionsBuilder.Options);
   }
