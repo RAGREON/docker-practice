c27a43b9d4af727c71fde4012123a290c82615d0
RAGREON <ragreon809@gmail.com>
2026-02-20 00:46:11 +0545

    change: IDbContextFactory to IDesignTimeDbContextFactory

--- Data/AppDbContextFactory.cs
+++ Data/AppDbContextFactory.cs
@@ -1,8 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
-public class AppDbContextFactory : IDbContextFactory<AppDbContext>
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
-  public AppDbContext CreateDbContext()
+  public AppDbContext CreateDbContext(string[] args)
   {
     var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
     optionsBuilder.UseNpgsql("Host:localhost;Database=MigrationBuildOnly;");
